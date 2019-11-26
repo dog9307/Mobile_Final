0dd2cfb3b9600b8fac4160fb39134111c9f2e5a3
dog9307 <dog9307@naver.com>
2019-11-26 18:29:09 +0900

    Level01
    1. 액티비티 추가 및 MainActivity 이름 변경
    2. Level01에서 사용할 ciricle리소스 생성
    3. circle리소스에서 사용할 애니메이션(scale) 생성
    => 거의 완성

---
+++ proj/app/src/main/java/com/example/mp_final/Level01Activity.java
@@ -0,0 +1,234 @@
+package com.example.mp_final;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class Level01Activity extends Activity {
+
+    private static final int    MARGIN = 200;
+    private static final int    TOTAL_MAX = 30;
+    private static final int    GAMEOVER_COUNT = 3;
+    private static final float  DELAY_MIN = 0.2f;
+    private static final float  DELAY_DECREASE = 0.05f;
+
+    private int _totalCircleCount;
+    private int _currentCircleCount;
+    private boolean _isGameOver;
+    private boolean _isGameClear;
+    private ConstraintLayout _circleArea;
+
+    private TextView _levelStatus;
+
+    private float _delayTime;
+
+    class Level_01_circle extends RelativeLayout
+    {
+        private ImageView _circle;
+        private RelativeLayout _instance;
+
+        public Level_01_circle(Context context) {
+            super(context);
+
+            _instance = this;
+            init(context);
+        }
+
+        private void init(Context c)
+        {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.level_01_circle, this, true);
+
+            _circle = (ImageView)this.findViewById(R.id.circle_image);
+            Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_bigger);
+            anim.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    _currentCircleCount++;
+                    _totalCircleCount++;
+
+                    if (_currentCircleCount > GAMEOVER_COUNT)
+                        _isGameOver = true;
+                }
+                @Override
+                public void onAnimationEnd(Animation animation) {}
+                @Override
+                public void onAnimationRepeat(Animation animation) {}
+            });
+            _circle.startAnimation(anim);
+
+            this.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    _currentCircleCount--;
+                    Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_smaller);
+                    anim.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {}
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            _circleArea.removeView(_instance);
+                        }
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {}
+                    });
+                    _circle.startAnimation(anim);
+
+                    if (_currentCircleCount <= 0 && _totalCircleCount >= TOTAL_MAX)
+                        _isGameClear = true;
+                }
+            });
+        }
+    }
+
+    private Handler _handle;
+    private Runnable _generator = new Runnable() {
+        @Override
+        public void run() {
+            try
+            {
+                if (_isGameOver)
+                {
+                    _levelStatus.setVisibility(View.VISIBLE);
+                    _levelStatus.setText("Game Over");
+                    _handle.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            _handle.removeCallbacks(_gameStarter);
+                            _handle.removeCallbacks(_generator);
+
+                            startActivity(new Intent(getApplication(), Level01Activity.class));
+                            finish();
+                        }
+                    }, 2000);
+                }
+                else if (_isGameClear)
+                {
+                    _levelStatus.setVisibility(View.VISIBLE);
+                    _levelStatus.setText("Game Clear!");
+                    _handle.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            _handle.removeCallbacks(_gameStarter);
+                            _handle.removeCallbacks(_generator);
+
+                            startActivity(new Intent(getApplication(), Level02Activity.class));
+                            finish();
+                        }
+                    }, 2000);
+                }
+                else
+                {
+                    if (_totalCircleCount < TOTAL_MAX)
+                        GenerateCircle();
+                }
+            }
+            finally {
+                _delayTime -= DELAY_DECREASE;
+                if (_delayTime < DELAY_MIN)
+                    _delayTime = DELAY_MIN;
+                    _handle.postDelayed(_generator, (long)(_delayTime * 1000));
+            }
+        }
+    };
+
+    private Runnable _gameStarter = new Runnable() {
+        @Override
+        public void run() {
+            if (_levelStatus.getText().toString().equals("3"))
+            {
+                _levelStatus.setText(2 + "");
+                _handle.postDelayed(_gameStarter, 1000);
+            }
+            else if (_levelStatus.getText().toString().equals("2"))
+            {
+                _levelStatus.setText(1 + "");
+                _handle.postDelayed(_gameStarter, 1000);
+            }
+            else if (_levelStatus.getText().toString().equals("1"))
+            {
+                _levelStatus.setText("Start!");
+                _handle.postDelayed(_gameStarter, 1000);
+            }
+            else
+            {
+                _levelStatus.setVisibility(View.GONE);
+                _handle.postDelayed(_generator, (long) (_delayTime * 1000));
+            }
+        }
+    };
+
+    private void StartRepeat()
+    {
+        _handle.postDelayed(_gameStarter, 1000);
+    }
+
+    private void GameReset()
+    {
+        _levelStatus.setText(3 + "");
+        _circleArea.removeAllViews();
+
+        _currentCircleCount = 0;
+        _totalCircleCount = 0;
+
+        _isGameOver = false;
+        _isGameClear = false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_01);
+
+        _circleArea = findViewById(R.id.level_01_circle_area);
+        _levelStatus = (TextView) findViewById(R.id.level_status);
+
+        GameReset();
+
+        _delayTime = 1.0f;
+        _handle = new Handler();
+        StartRepeat();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        _handle.removeCallbacks(_gameStarter);
+        _handle.removeCallbacks(_generator);
+    }
+
+    public void GenerateCircle()
+    {
+        Level_01_circle newCircle = new Level_01_circle(this);
+
+        Random rnd = new Random();
+        int x, y;
+        int min, max;
+        min = MARGIN;
+        max = _circleArea.getWidth() - MARGIN;
+        x = rnd.nextInt(max - min + 1) + min;
+
+        min = MARGIN;
+        max = _circleArea.getHeight() - MARGIN;
+        y = rnd.nextInt(max - min + 1) + min;
+
+        newCircle.setX(x);
+        newCircle.setY(y);
+
+        _circleArea.addView(newCircle);
+    }
+}
--- proj/app/src/main/java/com/example/mp_final/Level02Activity.java
+++ proj/app/src/main/java/com/example/mp_final/Level02Activity.java
@@ -1,14 +1,13 @@
 package com.example.mp_final;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Level02Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_level02);
     }
 }
